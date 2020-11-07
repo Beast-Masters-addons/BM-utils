bc77cbf987c8fe922c4f7ad3f4eef24b594ea8de
datagutten <datagutten@datagutten.net>
2020-11-07 16:05:41 +0100

    Add utils as class property to tests

--- tests/test_utils.lua
+++ tests/test_utils.lua
@@ -9,45 +9,45 @@ loadfile('wow_functions.lua')()
 loadfile('Util.lua')()
 loadfile('../libs/LibStub/LibStub.lua')()
 loadfile('../addon/utils.lua')()
-local utils = LibStub('BM-utils-1.0')
 
 _G['test'] = {}
 local test = _G['test']
+test.utils = _G['BMUtils']
 
 function test:testPrintf()
-    local text = utils:printf('Quad%s', 'duo')
+    local text = self.utils:printf('Quad%s', 'duo')
     lu.assertEquals(text, 'Quadduo')
 end
 
 function test:testSprintf()
-    local text = utils:sprintf('Quad%s', 'duo')
+    local text = self.utils:sprintf('Quad%s', 'duo')
     lu.assertEquals(text, 'Quadduo')
 end
 
 function test:testColorize()
-    local text = utils:colorize('Quadduo', 'ff3fbf3f')
+    local text = self.utils:colorize('Quadduo', 'ff3fbf3f')
     lu.assertEquals(text, '|cff3fbf3fQuadduo|r')
 end
 
 function test:testDefaultFontColor()
-    local _, r, g, b = utils:cprint('test')
+    local _, r, g, b = self.utils:cprint('test')
     lu.assertEquals(r, 1)
     lu.assertEquals(g, 1)
     lu.assertEquals(b, 1)
-    utils:SetDefaultFontColor(0, 0.25, 0.35)
-    _, r, g, b = utils:cprint('test')
+    self.utils:SetDefaultFontColor(0, 0.25, 0.35)
+    _, r, g, b = self.utils:cprint('test')
     lu.assertEquals(r, 0)
     lu.assertEquals(g, 0.25)
     lu.assertEquals(b, 0.35)
 end
 
 function test:testIsWoWClassic()
-    local is_classic = utils:IsWoWClassic()
+    local is_classic = self.utils:IsWoWClassic()
     lu.assertEquals(is_classic, true)
 end
 
 function test:testError()
-    local error, r, g, b = utils:error('this is bad')
+    local error, r, g, b = self.utils:error('this is bad')
     lu.assertEquals(error, 'this is bad')
     lu.assertEquals(r, 1)
     lu.assertEquals(g, 0)
@@ -55,64 +55,64 @@ function test:testError()
 end
 
 function test:testGetCharacterInfo()
-    local character, realm = utils:GetCharacterInfo()
+    local character, realm = self.utils:GetCharacterInfo()
     lu.assertEquals(character, 'Quadduo')
     lu.assertEquals(realm, 'MirageRaceway')
 
-    character, realm = utils:GetCharacterInfo('Quadgnome')
+    character, realm = self.utils:GetCharacterInfo('Quadgnome')
     lu.assertEquals(character, 'Quadgnome')
     lu.assertEquals(realm, 'MirageRaceway')
 end
 
 function test:testGetCharacterString()
-    local string = utils:GetCharacterString()
+    local string = self.utils:GetCharacterString()
     lu.assertEquals(string, 'Quadduo-MirageRaceway')
-    string = utils:GetCharacterString('Quadgnome')
+    string = self.utils:GetCharacterString('Quadgnome')
     lu.assertEquals(string, 'Quadgnome-MirageRaceway')
 end
 
 function test:testColor_to_rgb()
-    local r,g,b = utils:ColorToRGB({ r = 0.25, g = 0.75, b = 0.25 })
+    local r,g,b = self.utils:ColorToRGB({ r = 0.25, g = 0.75, b = 0.25 })
     lu.assertEquals(r, 63.75)
     lu.assertEquals(g, 191.25)
     lu.assertEquals(b, 63.75)
 end
 
 function test:testGenerateHexColor()
-    local hex = utils:GenerateHexColor(63.75, 191.25, 63.75)
+    local hex = self.utils:GenerateHexColor(63.75, 191.25, 63.75)
     lu.assertEquals(hex, 'ff3fbf3f')
 end
 
 function test:testColor_to_hex()
-    local hex = utils:ColorToHex({ r = 0.25, g = 0.75, b = 0.25 })
+    local hex = self.utils:ColorToHex({ r = 0.25, g = 0.75, b = 0.25 })
     lu.assertEquals(hex, 'ff3fbf3f')
 end
 
 function test:testDifficultyToNum()
-    local num = utils:DifficultyToNum("medium")
+    local num = self.utils:DifficultyToNum("medium")
     lu.assertEquals(num, 3)
 end
 
 function test:testSplitCharacterString()
-    local character, realm = utils:SplitCharacterString('Quadduo-MirageRaceway')
+    local character, realm = self.utils:SplitCharacterString('Quadduo-Mirage Raceway')
     lu.assertEquals(character, 'Quadduo')
-    lu.assertEquals(realm, 'MirageRaceway')
+    lu.assertEquals(realm, 'Mirage Raceway')
 end
 
 function test:testItemIdFromLink()
-    local id = utils:ItemIdFromLink('|cffffffff|Hitem:2318::::::::25:::::::|h[Light Leather]|h|r')
+    local id = self.utils:ItemIdFromLink('|cffffffff|Hitem:2318::::::::25:::::::|h[Light Leather]|h|r')
     lu.assertEquals(id, 2318)
 end
 
 function test:testItemNameFromLink()
-    local id = utils:ItemNameFromLink('|cffffffff|Hitem:2318::::::::25:::::::|h[Light Leather]|h|r')
+    local id = self.utils:ItemNameFromLink('|cffffffff|Hitem:2318::::::::25:::::::|h[Light Leather]|h|r')
     lu.assertEquals(id, 'Light Leather')
 end
 
 function test:testDifficultyColor()
-    local color = utils:DifficultyColor("optimal")
+    local color = self.utils:DifficultyColor("optimal")
     lu.assertEquals(color, { r = 1.00, g = 0.50, b = 0.25, font = "GameFontNormalLeftOrange" })
-    local mixin = utils:DifficultyColor("optimal", true)
+    local mixin = self.utils:DifficultyColor("optimal", true)
     local r, g, b = mixin:GetRGB()
     lu.assertEquals(r, 1.00)
     lu.assertEquals(g, 0.50)
@@ -120,7 +120,7 @@ function test:testDifficultyColor()
 end
 
 function test:testDifficultyColorText()
-    local text = utils:DifficultyColorText('colored', 'medium')
+    local text = self.utils:DifficultyColorText('colored', 'medium')
     lu.assertEquals(text, '|cffffff00colored|r')
 end
 
