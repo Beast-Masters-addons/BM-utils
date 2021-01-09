7afc341fac78fb2fa35de9498a924dee0e6beef8
datagutten <datagutten@datagutten.net>
2021-01-09 15:33:11 +0100

    Add GetItemQualityColor and BuildItemQualityColors dummy functions

--- tests/wow_functions.lua
+++ tests/wow_functions.lua
@@ -46,3 +46,17 @@ end
 function CastSpellByName(spellName)
     return spellName
 end
+
+function GetItemQualityColor(quality)
+    local colors = loadfile('wow-data/ColorData.lua')()
+    return colors[quality][1], colors[quality][2], colors[quality][3], colors[quality][4]
+end
+
+function BuildItemQualityColors()
+    ITEM_QUALITY_COLORS = { };
+    for i = 0, 8 do
+        local r, g, b = GetItemQualityColor(i);
+        local color = CreateColor(r, g, b, 1);
+        ITEM_QUALITY_COLORS[i] = { r = r, g = g, b = b, hex = color:GenerateHexColorMarkup(), color = color };
+    end
+end
