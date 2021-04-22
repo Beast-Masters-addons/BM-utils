2106852b96fe8930951eea987c267a8a55d4db7f
datagutten <datagutten@datagutten.net>
2021-04-22 21:22:34 +0200

    Update tests for TBC

--- tests/wow_functions.lua
+++ tests/wow_functions.lua
@@ -6,7 +6,7 @@ addon = {}
 C_Timer = {}
 function C_Timer:NewTicker(...) end
 
-if os.getenv('CLASSIC_VERSION') ~= nil then
+if os.getenv('GAME_VERSION') ~= 'retail' then
     loadfile('wow-ui-source/SharedXML/Util.lua')()
 else
     loadfile('wow-ui-source/SharedXML/Color.lua')()
@@ -14,8 +14,10 @@ else
 end
 
 function GetBuildInfo()
-    if os.getenv('CLASSIC_VERSION') ~= nil then
+    if os.getenv('GAME_VERSION') == 'classic' then
         return "1.13.2", 32600, "Nov 20 2019", 11302
+    elseif os.getenv('GAME_VERSION') == 'bc' then
+        return "2.5.1", 38364, "Apr 15 2021", 20501
     else
         return "9.0.1", "36492", "Oct 30 2020", 90001
     end
