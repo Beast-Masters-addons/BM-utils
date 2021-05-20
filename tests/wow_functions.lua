88d5714b2602312451e4f50095fbb1d4e78c958b
datagutten <datagutten@datagutten.net>
2021-05-20 17:46:54 +0200

    Add mixin functions for TBC and change interface branch names

--- tests/wow_functions.lua
+++ tests/wow_functions.lua
@@ -6,7 +6,10 @@ addon = {}
 C_Timer = {}
 function C_Timer:NewTicker(...) end
 
-if os.getenv('GAME_VERSION') ~= 'retail' then
+if os.getenv('GAME_VERSION') == 'classic' then
+    loadfile('wow-ui-source/SharedXML/Util.lua')()
+elseif os.getenv('GAME_VERSION') == 'bcc' then
+    loadfile('wow_mixin.lua')()
     loadfile('wow-ui-source/SharedXML/Util.lua')()
 else
     loadfile('wow-ui-source/SharedXML/Color.lua')()
