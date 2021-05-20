80a46823cb76a280633131ffa8ab33d8c85a2b75
datagutten <datagutten@datagutten.net>
2021-05-20 16:48:58 +0200

    Change to bcc in test dummy

--- tests/wow_functions.lua
+++ tests/wow_functions.lua
@@ -16,7 +16,7 @@ end
 function GetBuildInfo()
     if os.getenv('GAME_VERSION') == 'classic' then
         return "1.13.2", 32600, "Nov 20 2019", 11302
-    elseif os.getenv('GAME_VERSION') == 'bc' then
+    elseif os.getenv('GAME_VERSION') == 'bcc' then
         return "2.5.1", 38364, "Apr 15 2021", 20501
     else
         return "9.0.1", "36492", "Oct 30 2020", 90001
