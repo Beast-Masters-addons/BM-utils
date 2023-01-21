f8622c48ab579ea333d9969341ec89c4ba5defb3
datagutten <datagutten@datagutten.net>
2023-01-21 08:54:37 +0100

    Wrath Classic now uses new container API

--- addon/Container.lua
+++ addon/Container.lua
@@ -11,12 +11,12 @@ addon.container = {}
 ---@type BMUtilsContainer
 local container = addon.container
 
-local is_classic = _G.WOW_PROJECT_ID ~= _G.WOW_PROJECT_MAINLINE
+local is_classic_era = _G.WOW_PROJECT_ID == _G.WOW_PROJECT_CLASSIC
 
 ---Returns the total number of slots in the bag specified by the index.
 ---@param bagID int The slot containing the bag, e.g. 0 for backpack, etc.
 function container.GetContainerNumSlots(bagID)
-    if is_classic then
+    if is_classic_era then
         return _G.GetContainerNumSlots(bagID)
     else
         return _G.C_Container.GetContainerNumSlots(bagID)
@@ -24,7 +24,7 @@ function container.GetContainerNumSlots(bagID)
 end
 
 function container.GetContainerItemInfo(bagID, slot)
-    if is_classic then
+    if is_classic_era then
         local icon, itemCount, locked, quality, readable, lootable,
         itemLink, isFiltered, noValue, itemID, isBound = _G.GetContainerItemInfo(bagID, slot)
         return {
@@ -46,7 +46,7 @@ function container.GetContainerItemInfo(bagID, slot)
 end
 
 function container.PickupContainerItem(containerIndex, slotIndex)
-    if is_classic then
+    if is_classic_era then
         return _G.PickupContainerItem(containerIndex, slotIndex)
     else
         return _G.C_Container.PickupContainerItem(containerIndex, slotIndex)
