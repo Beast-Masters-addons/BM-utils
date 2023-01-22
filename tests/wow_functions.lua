36e009128f253e6001ea3bafed68d6037acc1d2f
datagutten <datagutten@datagutten.net>
2023-01-22 11:57:14 +0100

    Move function dummies to build utils

--- tests/wow_functions.lua
+++ tests/wow_functions.lua
@@ -21,38 +21,6 @@ end
 loadfile('wow-ui-source/Interface/SharedXML/Color.lua')()
 loadfile('wow-ui-source/Interface/SharedXML/Mixin.lua')()
 
-function GetBuildInfo()
-    if os.getenv('GAME_VERSION') == 'classic' then
-        return "1.13.2", 32600, "Nov 20 2019", 11302
-    elseif os.getenv('GAME_VERSION') == 'bcc' then
-        return "2.5.1", 38364, "Apr 15 2021", 20501
-    elseif os.getenv('GAME_VERSION') == 'wrath' then
-        return "3.4.0", 45327, "Aug 25 2022", 30400
-    else
-        return "9.0.1", "36492", "Oct 30 2020", 90001
-    end
-end
-
-function UnitName(unit)
-    if unit=="player" then
-        return "Quadduo"
-    end
-end
-
-function GetRealmName()
-    return "MirageRaceway"
-end
-
-function GetSpellInfo(spellId)
-    if spellId  == 186257 then
-        return "Aspect of the Cheetah", 132242, 0, 0, 0, 186257
-    end
-end
-
-function CastSpellByName(spellName)
-    return spellName
-end
-
 function GetItemQualityColor(quality)
     local colors = loadfile('wow-data/ColorData.lua')()
     return colors[quality][1], colors[quality][2], colors[quality][3], colors[quality][4]
